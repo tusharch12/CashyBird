2126378524ae08df46d60cb77929af2a31bc0d9d
tusharch12 <tusharch6577@gmail.com>
2025-05-21 22:16:31 +0530

    change db url

--- src/store/db.ts
+++ src/store/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const db = mongoose.connect('mongodb+srv://charmtechic:Illl6ZZaoJBdw4wK@cashybird.bpnydfs.mongodb.net/local_dev').then(()=>[
+const db = mongoose.connect('mongodb+srv://charmtechic:Illl6ZZaoJBdw4wK@cashybird.bpnydfs.mongodb.net/cashy_bird_db').then(()=>[
 console.log("Connection done !!!")
 ]).catch((err:any)=>{
 console.log("error ...",err);
